0ad4e384cc2297e15fc7fff4fef7f9f0011e9b97
kiratsubasa <55526871+kiratsubasa@users.noreply.github.com>
2021-04-27 15:49:31 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+#app
+  #nav
+    router-link.menu(to="/") Home 
+    router-link.menu(to="/website") Website
+    router-link.menu(to="/installation") Installation
+    router-link.menu(to="/graphic") Graphic
+    //- router-link.menu(to="/about") About
+    //- router-link.menu(to="/about") About
+  //- .menu
+  router-view
+</template>
+<style lang="sass">
+#app 
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  background-color: #40414F
+#nav 
+  width: 100vw
+  height: 10vh
+  // background-color: #aaa
+  position: fixed
+  display: flex
+  align-items: center
+  justify-content: left
+  z-index: 100
+#nav a 
+  font-weight: bold
+  color: #E0D8CC
+  text-decoration: none
+#nav a.router-link-exact-active 
+  color: #DEBB87
+.menu
+  font-size: 1.8vw
+  height: 36px
+  padding-left: 3.6vw
+  // line-height: 10vh
+</style>
